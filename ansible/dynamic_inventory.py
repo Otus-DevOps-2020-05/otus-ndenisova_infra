ba8490fb432344229ec732bb61e3d397175a0a3c
Nadezhda Denisova <rpnc@mail.ru>
2020-07-21 18:57:00 +0300

    ansible dz1

---
+++ ansible/dynamic_inventory.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import argparse
+import subprocess
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--list', action = 'store_true')
+args = parser.parse_args()
+
+if args.list:
+    p = subprocess.Popen(["cd ../terraform/stage; terraform show | grep external_ip_address_app; cd -"], stdout=subprocess.PIPE, shell=True)
+    appip=p.stdout.read().split('"')[1]
+
+    p = subprocess.Popen(["cd ../terraform/stage; terraform show | grep external_ip_address_db; cd -"], stdout=subprocess.PIPE, shell=True)
+    dbip=p.stdout.read().split('"')[1]
+
+    dd = '{"_meta":{"hostvars":{"appserver":{"ansible_host":"%s"},"dbserver":{"ansible_host":"%s"}}}, "all":{"children":["ungrouped","app","db"],"hosts":[],"vars":{}},"app":{"hosts":["appserver"]},"db":{"hosts":["dbserver"]}}'%(appip,dbip)
+    print json.dumps(json.loads(dd))
+
+else:
+    print json.dumps(json.loads('{"_meta": {"hostvars": {}}}'))
