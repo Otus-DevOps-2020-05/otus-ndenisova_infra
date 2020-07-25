7a71eb9e067563e863df27322e49c49df871c975
Nadezhda Denisova <rpnc@mail.ru>
2020-07-25 15:45:36 +0300

    DZ ansible-3

--- terraform/files/lb.tf
+++ terraform/files/lb.tf
@@ -9,6 +9,14 @@ resource "yandex_lb_network_load_balancer" "mylb" {
     }
   }
 
+  listener {
+    name = "my-listener-nginx"
+    port = 80
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+
   attached_target_group {
     target_group_id = "${yandex_lb_target_group.t-group.id}"
 
@@ -19,5 +27,13 @@ resource "yandex_lb_network_load_balancer" "mylb" {
         path = "/"
       }
     }
+    healthcheck {
+      name = "http"
+      http_options {
+        port = 80
+        path = "/"
+      }
+    }
+
   }
 }
